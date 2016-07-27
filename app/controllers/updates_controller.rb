8c8702855a773b85d1238b44318d317945abb8d7
nadirjin <gin.nader94@gmail.com>
2016-07-26 20:55:53 -0400

    Added Updates CRUD

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,4 +1,49 @@
 class UpdatesController < ApplicationController
+  before_action :find_update, only: [:show, :edit, :update, :destroy]
   def index
+  	@update = Update.new
+  end
+
+  def new
+  	@update = Update.new
+  end
+
+  def create
+  	@update = Update.new update_params
+
+  	if @update.save
+  		redirect_to @update, notice: "Update successfully saved"
+  	else
+  		render 'new', notice: "Update not saved."
+  	end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  	if @update.update update_params
+  		redirect_to @update, notice: "Update successfully saved."
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@update.destroy
+  	redirect_to updates_path
+  end
+
+  private
+
+  def update_params
+  	params.require(:update).permit(:content)
+  end
+
+  def find_update
+  	@update = Update.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Rails.application.routes.draw do
 
   get 'updates/index'
 
+  get 'updates/new'
+
+  get 'updates/show'
+
   get 'projects/show'
 
   get 'home/index'
 
   root 'home#index'
+
+  resources :updates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
